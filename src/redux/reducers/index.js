7f136d053470ffffb55b15cdc62e08fc45edbec5
Amen Ra <amen_ra@comcast.com>
2019-06-08 12:45:23 -0400

    Initial Commit

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,12 @@
+import { combineReducers } from "redux";
+import courses from "./courseReducer";
+import authors from "./authrorReducer";
+import apiCallsInProgress from "./apiStatusReducer";
+
+const rootReducer = combineReducers({
+  courses,
+  authors,
+  apiCallsInProgress
+});
+
+export default rootReducer;
